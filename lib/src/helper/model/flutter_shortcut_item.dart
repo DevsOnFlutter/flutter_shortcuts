3c0bb4479b79426cb059d91bd9ab2396bbf722c4
Divyanshu Shekhar <imdshekhar@gmail.com>
2021-05-25 10:03:27 +0530

    api documentation

--- lib/src/helper/model/flutter_shortcut_item.dart
+++ lib/src/helper/model/flutter_shortcut_item.dart
@@ -9,6 +9,27 @@ for more details.
 */
 
 class FlutterShortcutItem {
+  /// Create a flutter shortcut item.
+  /// Eg.
+  /// ```
+  /// const FlutterShortcutItem(
+  /// ```
+  /// ```
+  ///   id: "1",
+  /// ```
+  /// ```
+  ///   action: 'Home page action',
+  /// ```
+  /// ```
+  ///   shortLabel: 'Home Page',
+  /// ```
+  /// ```
+  ///   icon: 'assets/icons/home.png',
+  /// ```
+  /// ```
+  /// );
+  /// ```
+
   const FlutterShortcutItem({
     required this.id,
     required this.action,
@@ -17,18 +38,18 @@ class FlutterShortcutItem {
     this.icon,
   });
 
-  // Id of the shortcut
+  /// ID of the shortcut that differentiates it from other shortcuts.
   final String id;
 
-  // Action performed by the shortcut
+  /// Action performed by the shortcut
   final String action;
 
-  // Short label of the shortcut
+  /// Short label of the shortcut
   final String shortLabel;
 
-  // Long label of the shortcut
+  /// Long label of the shortcut
   final String? longLabel;
 
-  // Icon of the shortcut
+  /// Flutter asset path. Only Supports image files. Eg. .png/.jpg
   final String? icon;
 }
