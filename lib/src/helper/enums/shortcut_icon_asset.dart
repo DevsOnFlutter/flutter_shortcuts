76e50c06b7810988088969a8866d545340546655
Divyanshu Shekhar <imdshekhar@gmail.com>
2021-05-25 10:44:52 +0530

    enum created #4

---
+++ lib/src/helper/enums/shortcut_icon_asset.dart
@@ -0,0 +1,17 @@
+/* 
+
+              Copyright (c) 2021 divshekhar (Divyanshu Shekhar). 
+                            All rights reserved.
+
+The plugin is governed by the BSD-3-clause License. Please see the LICENSE file
+for more details.
+
+*/
+
+enum ShortcutIconAsset {
+  /// Creates Icon from the native resources `drawable` & `mipmap` directory.
+  androidAsset,
+
+  /// Creates Icon from the flutter resources `assets/icons/flutter.png`.
+  flutterAsset,
+}
